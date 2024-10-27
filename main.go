ec747be35bfae32e5cea57bd974379f7e8514903
Tim Rutte <tim.rutte.dev@gmail.com>
2024-10-27 20:03:40 +0100

    updated tf

--- main.go
+++ main.go
@@ -129,11 +129,11 @@ func healthHandler(ctx context.Context, request events.APIGatewayProxyRequest) (
 func InitDB() {
 	var err error
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_PORT"),
-		os.Getenv("MYSQL_DB"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	db, err = sql.Open("mysql", dsn)
